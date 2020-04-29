c413fc13aaa6dc4aa0869cafbb71d57a2d6b4b1a
jjchase <jjchase12@gmail.com>
2020-04-29 09:30:32 -0400

    second commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Declare an arrow function named randomDamage that has no parameters and returns a random integer between 1 and 10.
 
 const randomDamage = _ => Math.floor(Math.random() * 10) + 1;
-console.log(randomDamage());
+//console.log(randomDamage());
 
 //Declare an arrow function named chooseOption that has two parameters named opt1 and opt2. chooseOption does two things:
 //Declares and initializes a variable named randNum to either a 0 or 1, randomly.
@@ -12,7 +12,7 @@ const chooseOption = (opt1, opt2) => {
 
     return randNum === 0 ? opt1 : opt2;
 }
-console.log(chooseOption("option1", "option2"));
+//console.log(chooseOption("option1", "option2"));
 
 // Declare a function expression named attackPlayer that has one parameter named health which returns a number equal to health minus the product of the randomDamage function
 
@@ -21,23 +21,23 @@ const attackPlayer = function (health) {
 }
 
 
-console.log(attackPlayer(""));
+//console.log(attackPlayer(""));
 
 //Declare an arrow function named logHealth that has two parameters named player and health which has a console.log method to state the following message: “player health: health”.
 
-const logHealth = (player, health) => `${player} health: ${health}`;
-console.log(logHealth("Sam", 0));
+const logHealth = (player, health) => {
+    console.log(logHealth(`${player} health: ${health}`));
+}
 
 //Declare an arrow function named logDeath that has two parameters named winner and loser which has a console.log method to state the following message: “winner defeated loser”
-const logDeath = (winner, loser) => `${winner} defeated ${loser}`;
-console.log(logDeath("Jack", "Sam"));
+const logDeath = (winner, loser) =>
+    console.log(logDeath(`${winner} defeated ${loser}`));
 
 //Declare an arrow function named isDead that has one parameter named health which returns a boolean value of true or false based on the following condition: health <= 0;
 
-const isDead = (health) => {
-    return (`${health}` <= 0) ? true : false;
-}
-console.log(isDead());
+const isDead = (health) => health <= 0;
+
+//console.log(isDead());
 
 //Declare a function declaration named fight that has four parameters: 
 // player1 - this will hold the name of the first player
@@ -72,32 +72,29 @@ console.log(isDead());
 function fight(player1, player2, player1Health, player2Health) {
     while (true) {
         let attacker = chooseOption(player1, player2);
-        if (attacker = player1) {
+        if (attacker === player1) {
             player2Health = attackPlayer(player2Health);
-            console.log(logHealth(player2, player2Health));
-            isDead(player2Health)
-        }
-        if (isDead()) {
-            console.log(logDeath(player1, player2));
-            break;
+            logHealth(player2, player2Health);
+            if (isDead(player2Health)) {
+                logDeath(player1, player2);
+                break;
+            }
         }
         else {
             player1Health = attackPlayer(player1Health);
-            console.log(logHealth(player1, player1Health));
-            isDead(player1Health);
-        }
-        if (isDead()) {
-            console.log(logDeath(player2, player1));
-            break;
+            logHealth(player1, player1Health);
+            if (isDead(player1Health)) {
+                logDeath(player2, player1);
+                break;
+
+            }
 
         }
-
     }
 }
-
 // call the fight function with the required four parameters. You pick the names
 // and starting health. For example: player1: “Mitch”, player2: “Adam”, player1Health: 100,
 // player2Health: 100.
 
 
-//fight("Jack", "Sam", 100, 100)
+fight("Jack", "Sam", 100, 100)
